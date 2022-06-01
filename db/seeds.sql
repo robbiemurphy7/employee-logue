2f110c6da56418340137be2fd4324458ae7c2eaf
robbiemurphy <rmurphy987@gmail.com>
2022-06-01 01:56:32 -0500

    seeds.sql evening out employees and names

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,13 +8,9 @@ VALUES
 INSERT INTO role (title, salary, department_id)
 VALUES
   ('CSS Expert', 60000, 1),
-  ('HTML Renderer', 50000, 1),
   ('MongoDB Pro', 70000, 2),
-  ('Stripe Aficionado', 75000, 2),
-  ('MERN Creator', 160000, 3),
   ('React Lord', 125000, 3),
   ('Graphic Designer', 22000, 4),
-  ('Photoshop Wizard', 35000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
